9d9e51bd47c2bc929479f4430cd20e93b567cb21
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-09-13 22:56:08 +0200

    compte a rebours (presque) fonctionnel

--- index.js
+++ index.js
@@ -1,10 +1,57 @@
-// Créer la logique de compte à rebours
+let totalSeconds = 0;
+let minutes = 0;
+let seconds = 0;
+let active = false;
 
-choice.value = 5 minutes
+function timeDisplay() {
+  if (seconds < 10) {
+    countdownDisplay.textContent = minutes + " : 0" + seconds;
+  } else {
+    countdownDisplay.textContent = minutes + " : " + seconds;
+  }
+}
 
-choice.value * 60 = totalSeconds
+function over() {
+  if (active === false) {
+    setTimeout(() => {
+      countdownDisplay.style.visibility = "hidden";
+    }, 500);
+    setTimeout(() => {
+      countdownDisplay.style.visibility = "visible";
+      over();
+    }, 1000);
+  }
+}
 
-let minutes = Math.floor(totalSeconds / 60);
-let seconds = totalSeconds % 60
+function compteRebours() {
+  if (minutes === 0 && seconds === 0) {
+    active = false;
+    over();
+  } else if (seconds === 0) {
+    minutes -= 1;
+    seconds = 59;
+    timeDisplay();
+    setTimeout(() => {
+      compteRebours();
+    }, 1000);
+  } else {
+    seconds -= 1;
+    timeDisplay();
+    setTimeout(() => {
+      compteRebours();
+    }, 1000);
+  }
+}
 
-// Créer un événement à la validation du form pour lancer le compte à rebours
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  totalSeconds = choice.value * 60;
+  minutes = Math.floor(totalSeconds / 60);
+  seconds = totalSeconds % 60;
+  active = true;
+
+  timeDisplay();
+  setTimeout(() => {
+    compteRebours();
+  }, 1000);
+});
